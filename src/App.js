3994d86d1812c94cd460bf35ddad8bb2c4783b77
RawanAlakhras <ralakhras36@gmail.com>
2021-06-06 20:45:04 +0300

    comlete lab 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 class App extends React.Component{
   render(){
     return(
-      <h1>Rawan</h1>
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     )
   }
 }
